910f800fb1bc028744eef72916783f630c0323f5
Elena Lebedeva <lena4lebedeva@gmail.com>
2023-04-18 17:54:15 +0400

    first commit

--- src/test/java/ru/netology/service/cashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/cashbackHackServiceTest.java
@@ -1,17 +1,21 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
 
-class cashbackHackServiceTest {
+//import static org.junit.jupiter.api.Assertions.*;
+
+ public class cashbackHackServiceTest {
     @Test
     void calculateCashback(){
         cashbackHackService service = new cashbackHackService();
         int expected =100;
         int amount900 = 900;
         int actual = service.remain (amount900);
-        assertEquals(expected, actual);
+        assertEquals(actual,expected);
 
 
     }
