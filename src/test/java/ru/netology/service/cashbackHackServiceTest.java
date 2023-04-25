1144e0adca55d316092d5208de6ebd9618e02547
Elena Lebedeva <lena4lebedeva@gmail.com>
2023-04-25 21:33:42 +0400

    first commit

--- src/test/java/ru/netology/service/cashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/cashbackHackServiceTest.java
@@ -12,7 +12,7 @@ import static org.testng.Assert.assertEquals;
     @Test
     void calculateCashback(){
         cashbackHackService service = new cashbackHackService();
-        int expected =100;
+        int expected =1000;
         int amount900 = 1000;
         int actual = service.remain (amount900);
         assertEquals(actual,expected);
